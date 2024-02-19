4a65e83bd03f0940706d5fc5c1ad71ab351ee646
Soham-Chavan <chavansoham11@gmail.com>
2024-02-19 21:48:17 +0530

    removal of member feature added

--- src/components/dashboard/people.js
+++ src/components/dashboard/people.js
@@ -10,6 +10,8 @@ import supabase from '../../supabaseClient';
 import Stack from '@mui/joy/Stack';
 import Divider from '@mui/joy/Divider';
 import Chip from '@mui/joy/Chip';
+import Button from "@mui/joy/Button"
+import $ from "jquery"
 /* import "./people.css" */
 
 
@@ -19,6 +21,7 @@ import Chip from '@mui/joy/Chip';
 export function People({ classId }) {
     //creator
     const [creatorName, setCreatorName] = React.useState()
+    const [members, setMembers] = React.useState([])
     //all joined students.
 
     async function creator() {
@@ -41,11 +44,53 @@ export function People({ classId }) {
         if (error) {
             console.log(error)
         }
+
+        const getId = await supabase.auth.getUser()
+        const user_id = getId.data.user.id;
+        if (user_id !== data[0].created_by) {
+            $(`.remove-button`).hide()
+        }
     }
+
+    async function getMembers() {
+        try {
+            const { data, error } = await supabase
+                .from("class_members_duplicate")
+                .select("member_name")
+                .eq("classId", classId)
+            if (data) {
+                setMembers(data)
+            }
+            if (error) {
+                console.log(error)
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
 
     React.useEffect(() => {
         creator()
-    })
+        getMembers()
+    }, [])
+
+    if (members) {
+        console.log(members)
+    }
+
+    async function removeMember(memberName, index){
+        const {error} = await supabase
+            .from("class_members_duplicate")
+            .delete()
+            .eq("member_name", memberName)
+            .eq("classId", classId)
+        if (error) {
+            console.log(error)
+        }
+        $(`#list-item-${index}`).hide()
+    }
 
     return (
         <div style={{ display: "flex", justifyContent: "center" }}>
@@ -74,13 +119,25 @@ export function People({ classId }) {
                             </Typography>
                         </ListItemContent>
                     </ListItem>
-                    <Divider orientation="horizontal" sx={{ marginTop: "30px"}} >
+                    <Divider orientation="horizontal" sx={{ marginTop: "28px" }} >
                         <Chip variant="soft" color="danger" size="lg">
                             Students
                         </Chip>
                     </Divider>
+                    {members.map((member, index) => (
+                        <ListItem key={index} id={`list-item-${index}`} sx={{marginTop: "8px"}}>
+                            <ListItemDecorator>
+                                <Avatar src="/static/images/avatar/1.jpg" />
+                            </ListItemDecorator>
+                            <ListItemContent>
+                                <Typography level="title-sm">{member.member_name}</Typography>
+                                <Typography level="body-sm" noWrap>
+                                    Student
+                                </Typography>
+                            </ListItemContent><Button className="remove-button" id={`remove-button-${index}`} onClick={()=> removeMember(member.member_name, index)} color="danger" size="sm">Remove</Button>
+                        </ListItem>
+                    ))}
                 </List>
-               
             </Box>
         </div>
     )
